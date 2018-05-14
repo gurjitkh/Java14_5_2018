71b0154001ddd027ad11acc4963ad47a4d6af541
ekam <gurjitsingh1912@gmail.com>
2018-05-14 17:50:08 -0400

    Added AccountTest class

---
+++ src/Account.java
@@ -0,0 +1,12 @@
+public class Account {
+
+    private  String name;
+    // allow user to set account name to something else
+    public  void setName(String name){
+        this.name = name;
+    }
+    //
+    public String getName(){
+        return name;
+    }
+}
---
+++ src/AccountTest.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class AccountTest {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        //create an instance of account
+        Account myAccount = new Account();
+        //display the default account info.
+
+        System.out.printf("Intial bane is %s%n%n",myAccount.getName());
+
+       
+    }
+}
