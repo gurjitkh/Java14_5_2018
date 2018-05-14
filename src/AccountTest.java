3b750aa7a25ffc866329bb8cb9185ffd1364e835
ekam <gurjitsingh1912@gmail.com>
2018-05-14 17:50:45 -0400

    Added Driver code to accept the input

--- src/AccountTest.java
+++ src/AccountTest.java
@@ -10,6 +10,11 @@ public class AccountTest {
 
         System.out.printf("Intial bane is %s%n%n",myAccount.getName());
 
-       
+        //prompet the user name for an account name
+        System.out.println("Please Enter an account name: ");
+        String accName = input.nextLine();//Read input and save to accName
+        myAccount.setName(accName);
+
+        System.out.printf("Name of My Account is: %s %n",myAccount.getName());
     }
 }
