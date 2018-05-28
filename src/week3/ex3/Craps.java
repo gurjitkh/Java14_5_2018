3a695969332b5250b0ca1047a31afe2f89ada344
ekam <gurjitsingh1912@gmail.com>
2018-05-28 17:10:53 -0400

    craps completed

--- src/week3/ex3/Craps.java
+++ src/week3/ex3/Craps.java
@@ -16,7 +16,60 @@ public class Craps {
     private static final int SEVEN = 7;
     private static final int ELEVEN = 11;
     private static final int BOX_CARS = 12;
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        int myPoint = 0;
+        Status gameStatus;
+
+        int sumofDice =rollDice();
+
+        switch (sumofDice){
+            case SEVEN:
+            case ELEVEN:
+                gameStatus = Status.WON;
+                break;
+            case SNAKE_EYES:
+            case TREY:
+            case BOX_CARS:
+                gameStatus = Status.LOST;
+                break;
+                default:
+                    gameStatus = Status.CONTINUE;
+
+                    myPoint = sumofDice;
+                    System.out.printf("Point is %d %n ", myPoint);
+                    break;
+        }
+        while (gameStatus == Status.CONTINUE){
+            sumofDice = rollDice(); //roll dice again
+
+            if (sumofDice ==myPoint){
+                gameStatus = Status.WON;
+
+            }
+            else if (sumofDice == SEVEN){
+                gameStatus = Status.LOST;
+            }
+
+            if (gameStatus == Status.WON){
+                System.out.printf("Player Wins!!");
+            }
+            else {
+                System.out.printf("Player Losses!!");
+            }
+        }
+
+    }
+
+    public static int rollDice(){
+
+        int die1 = 1 + randomNumbers.nextInt(6);
+        int die2 = 1 + randomNumbers.nextInt(6);
+
+        int sum = die1 + die2;
+        //Display roll and sum to the user
+
+        System.out.printf("Player rolled %d + %d =%d %n",die1,die2,sum);
+        return sum;
     }
 }
---
+++ src/week3/ex4/Scope.java
@@ -0,0 +1,4 @@
+package week3.ex4;
+
+public class Scope {
+}
